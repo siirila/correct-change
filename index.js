b43823c72abc7073a60f5753e5098b507b6b9ca1
Jared Siirila <siirila@gmail.com>
2019-06-04 19:51:50 -0400

    Compute nickel change

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function correctChange(cost, cashProvided) {
     var costAsInteger = cost * 100;
     var cashAsInteger = cashProvided * 100;
-
-    var pennies = cashAsInteger - costAsInteger;
-    return {
-        pennies: pennies
+    var change = {
+        pennies: 0,
+        nickels: 0
     };
+    var changeRemaining = cashAsInteger - costAsInteger;
+
+    if (changeRemaining >= 5) {
+        change.nickels = 1;
+        changeRemaining -= 5;
+    }
+
+    change.pennies = changeRemaining;
+    return change;
 }
 
 module.exports.correctChange = correctChange;
--- index.test.js
+++ index.test.js
@@ -1,16 +1,39 @@
 var index = require('./index');
 var correctChange = index.correctChange;
 
+var expected;
+
+beforeEach(() => {
+    expected = {
+        pennies: 0,
+        nickels: 0
+    };
+});
+
 test('correctChange exists', () => {
   correctChange();
 });
 
 test('Provides no change when customer provided correct change', () => {
     var change = correctChange(1.00, 1.00);
-    expect(change).toEqual({ pennies: 0});
+    expect(change).toEqual(expected);
 });
 
-test('Provides one cent as change when customer paid with once cent more than required', () => {
+test('Provides one cent as change when customer paid with one cent more than required', () => {
     var change = correctChange(0.99, 1.00);
-    expect(change).toEqual({ pennies: 1});
+    expected.pennies = 1;
+    expect(change).toEqual(expected);
+});
+
+test('Provides a nickel as change when customer paid with 5 cents more than required', () => {
+    var change = correctChange(0.95, 1.00);
+    expected.nickels = 1;
+    expect(change).toEqual(expected);
+});
+
+test('Provides a nickel and penny as change when customer paid with 6 cents more than required', () => {
+    var change = correctChange(0.94, 1.00);
+    expected.pennies = 1;
+    expected.nickels = 1;
+    expect(change).toEqual(expected);
 });
