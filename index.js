2cde2dcd3ed854a36dec2ea55f908a388cab9d55
Jared Siirila <siirila@gmail.com>
2019-06-04 19:37:13 -0400

    Returns correct pennies

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function correctChange() {
+function correctChange(cost, cashProvided) {
+    var costAsInteger = cost * 100;
+    var cashAsInteger = cashProvided * 100;
 
+    var pennies = cashAsInteger - costAsInteger;
+    return {
+        pennies: pennies
+    };
 }
 
 module.exports.correctChange = correctChange;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,16 @@
-const index = require('./index');
+var index = require('./index');
+var correctChange = index.correctChange;
 
 test('correctChange exists', () => {
-  index.correctChange();
+  correctChange();
+});
+
+test('Provides no change when customer provided correct change', () => {
+    var change = correctChange(1.00, 1.00);
+    expect(change).toEqual({ pennies: 0});
+});
+
+test('Provides one cent as change when customer paid with once cent more than required', () => {
+    var change = correctChange(0.99, 1.00);
+    expect(change).toEqual({ pennies: 1});
 });
